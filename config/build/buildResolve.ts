3ca4334bf32be9e7e1e68ab7679698c7ee03c17d
Tatiana Roslyakova <tat-rs@yandex.ru>
2023-10-01 19:15:26 +0300

    fix: исправить buildResolve

--- config/build/buildResolve.ts
+++ config/build/buildResolve.ts
@@ -5,6 +5,7 @@ export function buildResolve(options: BuildOptions):webpack.ResolveOptions {
   return {
     extensions: ['.tsx', '.ts', '.js'],
     preferAbsolute: true,
+    mainFiles: ['index'],
     alias: {},
     modules: [
       options.paths.src, 'node_modules'
