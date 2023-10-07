672827210617e36255b8fca3b78597dc343023b2
Tatiana Roslyakova <tat-rs@yandex.ru>
2023-10-07 15:16:35 +0300

    feat: настроить AppRouter

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,7 @@
-import { Suspense} from "react";
-import { Route, Routes } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { useTheme } from "./providers/ThemeProvider";
 import { classNames } from "shared/lib/classNames/classNames";
-import { MainPage } from "pages/MainPage";
-import { AboutPage } from "pages/AboutPage";
+import AppRouter from "./router/ui/AppRouter";
 import "./styles/index.scss";
 
 const App = () => {
@@ -14,12 +11,7 @@ const App = () => {
     <div className={classNames('app', {}, [theme])}>
       <Link to={'/'}>На главную</Link>
       <Link to={'/about'}>О странце</Link>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path={'/'} element={<MainPage />} />
-          <Route path={'/about'} element={<AboutPage />} />
-        </Routes>
-      </Suspense>
+      <AppRouter />
       <button type="button" onClick={toggleTheme}>TOGGLE</button>
     </div>
   )
